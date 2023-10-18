6f3650758258145222e79786ec0520e10d63bca3
Vincent Roseberry <rosbo@google.com>
2023-10-18 21:11:44 +0000

    Do not use test library

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,6 @@
+import os
 import unittest
-from test.support.os_helper import EnvironmentVarGuard
+from unittest import mock
 
 from kagglehub.config import CACHE_FOLDER_ENV_VAR_NAME, DEFAULT_CACHE_FOLDER, get_cache_folder
 
@@ -8,8 +9,6 @@ class TestConfig(unittest.TestCase):
     def test_get_cache_folder_default(self):
         self.assertEqual(DEFAULT_CACHE_FOLDER, get_cache_folder())
 
+    @mock.patch.dict(os.environ, {CACHE_FOLDER_ENV_VAR_NAME: "/test_cache"})
     def test_get_cache_folder_environment_var_override(self):
-        env = EnvironmentVarGuard()
-        env.set(CACHE_FOLDER_ENV_VAR_NAME, "/test_cache")
-        with env:
-            self.assertEqual("/test_cache", get_cache_folder())
+        self.assertEqual("/test_cache", get_cache_folder())
