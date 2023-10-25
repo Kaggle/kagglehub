4b2f1ab2815320a2c183838afb7a004943146491
Vincent Roseberry <rosbo@google.com>
2023-10-25 17:50:01 +0000

    tarfile.extractall compatibility

--- src/kagglehub/http_resolver.py
+++ src/kagglehub/http_resolver.py
@@ -1,5 +1,6 @@
 import os
 import tarfile
+import sys
 import tempfile
 from typing import Optional
 
@@ -44,8 +45,7 @@ class HttpResolver(Resolver):
 
                 # Extract all files to this directory.
                 with tarfile.open(archive_file.name) as f:
-                    # Use 'data' filter to prevent dangerous security issues.
-                    f.extractall(out_path, filter="data")
+                    _tar_extract_all(f, out_path)
 
         mark_as_complete(h, path)
         return out_path
@@ -53,3 +53,13 @@ class HttpResolver(Resolver):
 
 def _build_download_url_path(h: ModelHandle):
     return f"models/{h.owner}/{h.model}/{h.framework}/{h.variation}/{h.version}/download"
+
+def _tar_extract_all(f: tarfile.TarFile, out_path: str):
+    if (sys.version_info.major, sys.version_info.minor) >= (3, 8, 17):
+        # Use 'data' filter to prevent dangerous security issues.
+        f.extractall(out_path, filter="data")
+    else:
+        # Older Python version do not support the `filter` parameter.
+        # Model archives are created by Kaggle via the Databundle Worker and don't
+        # include destination outside the output path.    
+        f.extractall(out_path)
