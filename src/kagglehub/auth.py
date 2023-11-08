f0ac3d1745296dc928438c5aa3e340de14770b1e
Vincent Roseberry <rosbo@google.com>
2023-11-08 09:01:24 -0800

    Remove extra slash in /kaggle/input path. (#21)
    https://screenshot.googleplex.com/7mQMShJYGZPqk8d

--- src/kagglehub/auth.py
+++ src/kagglehub/auth.py
@@ -1,2 +1,19 @@
+import json
+from kagglehub.config import CREDENTIALS_JSON_KEY, CREDENTIALS_JSON_USERNAME, KaggleApiCredentials, _get_kaggle_credentials_file
+
+
 def login():
-    raise NotImplementedError()
+    """Prompt the user for their Kaggle username and API key and save them to a global variable."""
+
+    username = input("Enter your Kaggle username: ")
+    api_key = input("Enter your Kaggle API key: ")
+
+    global _kaggle_credentials
+    _kaggle_credentials = KaggleApiCredentials(username=username, key=api_key)
+
+    # Save the credentials to a file for persistence
+    creds_filepath = _get_kaggle_credentials_file()
+    with open(creds_filepath, "w") as creds_json_file:
+        json.dump({CREDENTIALS_JSON_USERNAME: username, CREDENTIALS_JSON_KEY: api_key}, creds_json_file)
+
+    print("You are now logged in to Kaggle Hub.")
