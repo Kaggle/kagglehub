c86f02368d7b3a14394db3e873116937ff11ed06
Mohamed Amin <aminmohamed@google.com>
2023-11-16 20:00:20 +0000

    validation

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -19,6 +19,16 @@ class TestAuth(BaseTestCase):
         self.assertEqual("lastplacelarry", get_kaggle_credentials().username)
         self.assertEqual("some-key", get_kaggle_credentials().key)
 
+    def test_login_updates_global_credentials_no_validation(self):
+        # Simulate user input for credentials
+        with mock.patch("builtins.input") as mock_input:
+            mock_input.side_effect = ["lastplacelarry", "some-key"]
+            kagglehub.login(validate_credentials=False)
+
+        # Verify that the global variable contains the updated credentials
+        self.assertEqual("lastplacelarry", get_kaggle_credentials().username)
+        self.assertEqual("some-key", get_kaggle_credentials().key)
+
     def test_set_kaggle_credentials_raises_error_with_empty_username(self):
         with self.assertRaises(ValueError):
             with mock.patch("builtins.input") as mock_input:
@@ -36,3 +46,9 @@ class TestAuth(BaseTestCase):
             with mock.patch("builtins.input") as mock_input:
                 mock_input.side_effect = ["", ""]
                 kagglehub.login()
+
+    def test_login_raises_requests_exceptions_with_invalid_url(self):
+        with mock.patch("requests.get") as mock_get:
+            mock_get.side_effect = requests.exceptions.ConnectionError()
+            with self.assertRaises(requests.exceptions.ConnectionError):
+                kagglehub.login(validate_credentials=True)
