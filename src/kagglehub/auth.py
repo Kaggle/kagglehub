589fe644bc2f88e3adbc84834bceda5f89bfe621
Mohamed Amin <aminmohamed@google.com>
2023-11-16 20:00:20 +0000

    validation

--- src/kagglehub/auth.py
+++ src/kagglehub/auth.py
@@ -1,11 +1,14 @@
 import logging
 
+import requests
+
+from kagglehub.clients import KaggleApiV1Client
 from kagglehub.config import set_kaggle_credentials
 
 logger = logging.getLogger(__name__)
 
 
-def login():
+def login(validate_credentials=True):  # noqa: FBT002
     """Prompt the user for their Kaggle username and API key and save them globally."""
 
     username = input("Enter your Kaggle username: ")
@@ -14,3 +17,19 @@ def login():
     set_kaggle_credentials(username=username, api_key=api_key)
 
     logger.info("Kaggle credentials set.")
+
+    if validate_credentials is False:
+        return
+
+    try:
+        api_client = KaggleApiV1Client()
+        api_client.get("/hello")
+        logger.info("Kaggle credentials successfully validated.")
+    except requests.exceptions.HTTPError as e:
+        invalid_credentials_error = 403
+        if e.response.status_code == invalid_credentials_error:
+            logger.error(
+                "Invalid Kaggle credentials. You can check your credentials on the [Kaggle settings page](https://www.kaggle.com/settings/account)."
+            )
+        else:
+            logger.warning("Unable to validate Kaggle credentials at this time.")
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,19 +1,55 @@
+import http.server
+import json
 import logging
 from unittest import mock
+
+import requests
 
 import kagglehub
 from kagglehub.config import get_kaggle_credentials
 from tests.fixtures import BaseTestCase
 
+from .utils import create_test_http_server
+
 logger = logging.getLogger(__name__)
+
+
+class KaggleAPIHandler(http.server.BaseHTTPRequestHandler):
+    def do_HEAD(self):  # noqa: N802
+        self.send_response(200)
+
+    def do_GET(self):  # noqa: N802
+        if self.path == "/hello":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps({"message": "Hello from test server!"}), "utf-8"))
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "application/text")
+            self.send_header("Content-Length", 0)
+            self.end_headers()
+            self.wfile.write(bytes("", "utf-8"))
 
 
 class TestAuth(BaseTestCase):
     def test_login_updates_global_credentials(self):
         # Simulate user input for credentials
+
+        with create_test_http_server(KaggleAPIHandler):
+            with mock.patch("builtins.input") as mock_input:
+                mock_input.side_effect = ["lastplacelarry", "some-key"]
+                kagglehub.login()
+
+            # Verify that the global variable contains the updated credentials
+            self.assertEqual("lastplacelarry", get_kaggle_credentials().username)
+            self.assertEqual("some-key", get_kaggle_credentials().key)
+
+    def test_login_updates_global_credentials_no_validation(self):
+        # Simulate user input for credentials
         with mock.patch("builtins.input") as mock_input:
             mock_input.side_effect = ["lastplacelarry", "some-key"]
-            kagglehub.login()
+            kagglehub.login(validate_credentials=False)
 
         # Verify that the global variable contains the updated credentials
         self.assertEqual("lastplacelarry", get_kaggle_credentials().username)
