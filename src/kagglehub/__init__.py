64280dded1d34f0c91d9075bb4852573c7f5dceb
Vincent Roseberry <rosbo@google.com>
2024-01-31 14:19:22 -0800

    Prepare 0.1.8 release (#81)

--- src/kagglehub/__init__.py
+++ src/kagglehub/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 import kagglehub.logging  # configures the library logger.
 from kagglehub import colab_cache_resolver, http_resolver, kaggle_cache_resolver, registry
