4845a17740f207aabf725010d5846da0fabc72d0
Vincent Roseberry <rosbo@google.com>
2024-05-30 23:03:01 +0000

    Use test cache

--- integration_tests/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-from pathlib import Path
-
-# Ensures integration tests do not try to write into regular cache directory.
-os.environ["KAGGLEHUB_CACHE"] = os.path.join(Path.home(), ".cache", "kagglehub-integration-tests")
--- integration_tests/test_model_download.py
+++ integration_tests/test_model_download.py
@@ -1,12 +1,11 @@
 import os
-import shutil
 import unittest
 from typing import List
 
 from requests import HTTPError
 
+from integration_tests.utils import create_test_cache
 from kagglehub import model_download
-from kagglehub.config import get_cache_folder
 
 HANDLE = "keras/bert/keras/bert_tiny_en_uncased/2"
 
@@ -46,60 +45,60 @@ class TestModelDownload(unittest.TestCase):
 
             self.assertGreater(os.path.getsize(file_path), 0, f"File {file} is empty")
 
-    def setUp(self) -> None:
-        # Clear the cache folder
-        # Note: In unit tests, we use a tempfile.TemporaryDirectory. For integration tests, let's use a real folder.
-        shutil.rmtree(get_cache_folder(), ignore_errors=True)
-
     def test_model_versioned_succeeds(self) -> None:
-        actual_path = model_download(HANDLE)
+        with create_test_cache():
+            actual_path = model_download(HANDLE)
 
-        expected_files = [
-            "assets/tokenizer/vocabulary.txt",
-            "./config.json",
-            "./metadata.json",
-            "./model.weights.h5",
-            "./tokenizer.json",
-        ]
-        self.assert_files(actual_path, expected_files)
+            expected_files = [
+                "assets/tokenizer/vocabulary.txt",
+                "./config.json",
+                "./metadata.json",
+                "./model.weights.h5",
+                "./tokenizer.json",
+            ]
+            self.assert_files(actual_path, expected_files)
 
     def test_model_unversioned_succeeds(self) -> None:
-        unversioned_handle = "keras/bert/keras/bert_tiny_en_uncased"
-        actual_path = model_download(unversioned_handle)
+        with create_test_cache():
+            unversioned_handle = "keras/bert/keras/bert_tiny_en_uncased"
+            actual_path = model_download(unversioned_handle)
 
-        expected_files = [
-            "assets/tokenizer/vocabulary.txt",
-            "./config.json",
-            "./metadata.json",
-            "./model.weights.h5",
-            "./tokenizer.json",
-        ]
-        self.assert_files(actual_path, expected_files)
+            expected_files = [
+                "assets/tokenizer/vocabulary.txt",
+                "./config.json",
+                "./metadata.json",
+                "./model.weights.h5",
+                "./tokenizer.json",
+            ]
+            self.assert_files(actual_path, expected_files)
 
     def test_download_private_model_succeeds(self) -> None:
-        actual_path = model_download("integrationtester/test-private-model/pyTorch/b0")
+        with create_test_cache():
+            actual_path = model_download("integrationtester/test-private-model/pyTorch/b0")
 
-        expected_files = [
-            "./efficientnet-b0.pth",
-        ]
+            expected_files = [
+                "./efficientnet-b0.pth",
+            ]
 
-        self.assert_files(actual_path, expected_files)
+            self.assert_files(actual_path, expected_files)
 
     def test_download_archive_model_many_files_succeeds(self) -> None:
-        # If the model has > 25 files, we download the archive and uncompress it locally.
-        actual_path = model_download("integrationtester/test-private-model/keras/many-files-with-subdirectories/1")
+        with create_test_cache():
+            # If the model has > 25 files, we download the archive and uncompress it locally.
+            actual_path = model_download("integrationtester/test-private-model/keras/many-files-with-subdirectories/1")
 
-        expected_files = [f"assets/{i}.txt" for i in range(1, 26)] + [
-            "./config.json",
-            "./model.keras",
-        ]
-        self.assert_files(actual_path, expected_files)
+            expected_files = [f"assets/{i}.txt" for i in range(1, 26)] + [
+                "./config.json",
+                "./model.keras",
+            ]
+            self.assert_files(actual_path, expected_files)
 
     def test_download_multiple_files(self) -> None:
-        file_paths = ["tokenizer.json", "config.json"]
-        for p in file_paths:
-            actual_path = model_download(HANDLE, path=p)
-            self.assert_files(actual_path, [p])
+        with create_test_cache():
+            file_paths = ["tokenizer.json", "config.json"]
+            for p in file_paths:
+                actual_path = model_download(HANDLE, path=p)
+                self.assert_files(actual_path, [p])
 
     def test_download_with_incorrect_file_path(self) -> None:
         incorrect_path = "nonexistent/file/path"
