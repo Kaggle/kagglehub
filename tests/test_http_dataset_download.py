8224de2929e386efc8631da7add89c07e605e96a
Jonathan Ward <jeward@google.com>
2024-06-05 17:31:03 +0000

    Add more tests

--- tests/test_http_dataset_download.py
+++ tests/test_http_dataset_download.py
@@ -82,3 +82,15 @@ class TestHttpDatasetDownload(BaseTestCase):
     def test_versioned_dataset_download_with_path(self) -> None:
         with create_test_cache() as d:
             self._download_test_file_and_assert_downloaded(d, VERSIONED_DATASET_HANDLE)
+
+    def test_versioned_dataset_full_download_with_file_already_cached(self) -> None:
+        with create_test_cache() as d:
+            # Download a single file first
+            kagglehub.dataset_download(VERSIONED_DATASET_HANDLE, path=TEST_FILEPATH)
+            self._download_dataset_and_assert_downloaded(d, VERSIONED_DATASET_HANDLE, EXPECTED_DATASET_SUBDIR)
+
+    def test_unversioned_dataset_full_download_with_file_already_cached(self) -> None:
+        with create_test_cache() as d:
+            # Download a single file first
+            kagglehub.dataset_download(UNVERSIONED_DATASET_HANDLE, path=TEST_FILEPATH)
+            self._download_dataset_and_assert_downloaded(d, UNVERSIONED_DATASET_HANDLE, EXPECTED_DATASET_SUBDIR)
