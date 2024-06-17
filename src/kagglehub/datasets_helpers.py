219f9fd418c63bf4cba4ee60eb20e2af2396fc1a
Jonathan Ward <jeward@google.com>
2024-06-17 22:57:36 +0000

    fix post url

--- src/kagglehub/datasets_helpers.py
+++ src/kagglehub/datasets_helpers.py
@@ -2,7 +2,7 @@ import logging
 from http import HTTPStatus
 from typing import List
 
-from kagglehub.clients import KaggleApiV1Client
+from kagglehub.clients import BackendError, KaggleApiV1Client
 from kagglehub.exceptions import KaggleApiHTTPError
 from kagglehub.gcs_upload import UploadDirectoryInfo
 from kagglehub.handle import DatasetHandle
@@ -15,6 +15,7 @@ def _create_dataset(dataset_handle: DatasetHandle, files_and_directories: Upload
         "ownerSlug": dataset_handle.owner,
         "title": dataset_handle.dataset,
         "files": [{"token": file_token} for file_token in files_and_directories.files],
+        "isPrivate": True
     }
 
     api_client = KaggleApiV1Client()
@@ -25,29 +26,22 @@ def _create_dataset(dataset_handle: DatasetHandle, files_and_directories: Upload
 
 
 def _create_dataset_version(dataset_handle: DatasetHandle, files_and_directories: UploadDirectoryInfo, version_notes: str = "") -> None:
-    serialized_data = files_and_directories.serialize()
     data = {
         "versionNotes": version_notes,
         "files": [{"token": file_token} for file_token in files_and_directories.files],
-        "directories": serialized_data["directories"],
     }
     api_client = KaggleApiV1Client()
-    api_client.post(f"/datasets/create/{dataset_handle.version}/{dataset_handle.owner}/{dataset_handle.dataset}", data)
+    api_client.post(f"/datasets/create/version/{dataset_handle.owner}/{dataset_handle.dataset}", data)
     logger.info(f"Your dataset has been created.\nFiles are being processed...\nSee at: {dataset_handle.to_url()}")
 
 
 def create_dataset_or_version(dataset_handle: DatasetHandle, files: UploadDirectoryInfo, version_notes: str = "") -> None:
     try:
-        api_client = KaggleApiV1Client()
-        api_client.get(f"/view/datasets/{dataset_handle}", dataset_handle)
-        # the instance exists, create a new version.
-        _create_dataset_version(dataset_handle, files, version_notes)
-    except KaggleApiHTTPError as e:
-        if e.response is not None and (
-            e.response.status_code == HTTPStatus.NOT_FOUND  # noqa: PLR1714
-            or e.response.status_code == HTTPStatus.FORBIDDEN
-        ):
-            _create_dataset(dataset_handle, files)
+        _create_dataset(dataset_handle, files)
+    except BackendError as e:
+        if e.error_code == None:  # There is no CONFLICT error for datasets AFAIK
+            # Dataset already exists, creating a new version instead.
+            _create_dataset_version(dataset_handle, files, version_notes)
         else:
             raise (e)
 
