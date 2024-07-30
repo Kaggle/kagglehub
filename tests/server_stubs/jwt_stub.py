7e466ed977985c99527c61dce2bf39db473ac2e3
Jonathan Ward <jeward@google.com>
2024-07-30 17:26:32 +0000

    fix jwt_stub

--- tests/server_stubs/jwt_stub.py
+++ tests/server_stubs/jwt_stub.py
@@ -33,53 +33,50 @@ def error(e: Exception):  # noqa: ANN201
 
 
 @app.route("/kaggle-jwt-handler/AttachDatasourceUsingJwtRequest", methods=["POST"])
-def attach_dataset_datasource_using_jwt_request() -> ResponseReturnValue:
+def attach_datasource_using_jwt_request() -> ResponseReturnValue:
     data = request.get_json()
-    dataset_ref = data["datasetRef"]
-    version_number = LATEST_DATASET_VERSION
-    if "VersionNumber" in dataset_ref:
-        version_number = dataset_ref["VersionNumber"]
-    mount_slug = f"{dataset_ref['DatasetSlug']}"
-    # # Load the files
-    cache_mount_folder = os.getenv(KAGGLE_CACHE_MOUNT_FOLDER_ENV_VAR_NAME)
-    base_path = f"{cache_mount_folder}/{mount_slug}"
-    os.makedirs(base_path, exist_ok=True)
-    Path(f"{base_path}/foo.txt").touch()
-    if version_number == LATEST_DATASET_VERSION:
-        # The latest version has an extra file.
-        Path(f"{base_path}/bar.csv").touch()
-    data = {
-        "wasSuccessful": True,
-        "result": {
-            "mountSlug": mount_slug,
-        },
-    }
-    return jsonify(data), 200
-
-
-@app.route("/kaggle-jwt-handler/AttachDatasourceUsingJwtRequest", methods=["POST"])
-def attach_model_datasource_using_jwt_request() -> ResponseReturnValue:
-    data = request.get_json()
-    model_ref = data["modelRef"]
-    version_number = LATEST_MODEL_VERSION
-    if "VersionNumber" in model_ref:
-        version_number = model_ref["VersionNumber"]
-    mount_slug = f"{model_ref['ModelSlug']}/{model_ref['Framework']}/{model_ref['InstanceSlug']}/{version_number}"
-    # # Load the files
-    cache_mount_folder = os.getenv(KAGGLE_CACHE_MOUNT_FOLDER_ENV_VAR_NAME)
-    base_path = f"{cache_mount_folder}/{mount_slug}"
-    os.makedirs(base_path, exist_ok=True)
-    Path(f"{base_path}/config.json").touch()
-    if version_number == LATEST_MODEL_VERSION:
-        # The latest version has an extra file.
-        Path(f"{base_path}/model.keras").touch()
-    data = {
-        "wasSuccessful": True,
-        "result": {
-            "mountSlug": mount_slug,
-        },
-    }
-    return jsonify(data), 200
+    if "modelRef" in data:
+        model_ref = data["modelRef"]
+        version_number = LATEST_MODEL_VERSION
+        if "VersionNumber" in model_ref:
+            version_number = model_ref["VersionNumber"]
+        mount_slug = f"{model_ref['ModelSlug']}/{model_ref['Framework']}/{model_ref['InstanceSlug']}/{version_number}"
+        # # Load the files
+        cache_mount_folder = os.getenv(KAGGLE_CACHE_MOUNT_FOLDER_ENV_VAR_NAME)
+        base_path = f"{cache_mount_folder}/{mount_slug}"
+        os.makedirs(base_path, exist_ok=True)
+        Path(f"{base_path}/config.json").touch()
+        if version_number == LATEST_MODEL_VERSION:
+            # The latest version has an extra file.
+            Path(f"{base_path}/model.keras").touch()
+        data = {
+            "wasSuccessful": True,
+            "result": {
+                "mountSlug": mount_slug,
+            },
+        }
+        return jsonify(data), 200
+    elif "datasetRef" in data:
+        dataset_ref = data["datasetRef"]
+        version_number = LATEST_DATASET_VERSION
+        if "VersionNumber" in dataset_ref:
+            version_number = dataset_ref["VersionNumber"]
+        mount_slug = f"{dataset_ref['DatasetSlug']}"
+        # # Load the files
+        cache_mount_folder = os.getenv(KAGGLE_CACHE_MOUNT_FOLDER_ENV_VAR_NAME)
+        base_path = f"{cache_mount_folder}/{mount_slug}"
+        os.makedirs(base_path, exist_ok=True)
+        Path(f"{base_path}/foo.txt").touch()
+        if version_number == LATEST_DATASET_VERSION:
+            # The latest version has an extra file.
+            Path(f"{base_path}/bar.csv").touch()
+        data = {
+            "wasSuccessful": True,
+            "result": {
+                "mountSlug": mount_slug,
+            },
+        }
+        return jsonify(data), 200
 
 
 @contextmanager
