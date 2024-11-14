70822f20cc9e3c1cf1c6d7a345ee89365bbdc3d3
Vincent Roseberry <rosbo@google.com>
2024-11-14 22:58:05 +0000

    fmt

--- integration_tests/test_dataset_upload.py
+++ integration_tests/test_dataset_upload.py
@@ -9,10 +9,10 @@ from tempfile import TemporaryDirectory
 from typing import TypeVar
 
 from kagglehub import dataset_upload, datasets_helpers
-from kagglehub.config import get_kaggle_credentials
 from kagglehub.clients import KaggleApiV1Client
-from kagglehub.http_resolver import _get_current_version
+from kagglehub.config import get_kaggle_credentials
 from kagglehub.handle import parse_dataset_handle
+from kagglehub.http_resolver import _get_current_version
 
 logger = logging.getLogger(__name__)
 
@@ -54,7 +54,7 @@ class TestDatasetUpload(unittest.TestCase):
             self.assertTrue(dataset_upload_and_wait(self.handle, self.temp_dir))
 
             # Create Version
-            #dataset_upload(self.handle, self.temp_dir, "new version")
+            # dataset_upload(self.handle, self.temp_dir, "new version")
 
     def test_dataset_upload_directory(self) -> None:
         with TemporaryDirectory() as temp_dir:
@@ -118,19 +118,22 @@ class TestDatasetUpload(unittest.TestCase):
         time.sleep(5)  # hacky. Need to wait until a dataset is ready to be deleted.
         datasets_helpers.dataset_delete(self.owner_slug, self.dataset_slug)
 
-# TODO(b/379171781): Remove waiting logic once uploading a new version while the first version is being processed is supported.
+
+# TODO(b/379171781): Remove waiting logic once uploading a new version while the first version is being processed is
+# supported.
 def dataset_upload_and_wait(handle: str, local_dataset_dir: str, version_notes: str = "") -> bool:
     dataset_upload(handle, local_dataset_dir, version_notes)
     h = parse_dataset_handle(handle)
+    time.sleep(1)
     client = KaggleApiV1Client()
 
     max_attempts = 10
-    for attempt in range(max_attempts):
+    for _attempt in range(max_attempts):
         try:
-            time.sleep(5)
             if _get_current_version(client, h) > 0:
                 return True
-        except:
-            print(f"Attempt #{attempt} failed. Waiting some more for processing to finish")
+        except Exception:
+            # wait a bit before checking for completion again.
+            time.sleep(5)
 
     return False
