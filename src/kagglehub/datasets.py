84b798959b09ef22adbd38589e4ea11651b44e8c
Goeff Thomas <goeff@google.com>
2024-12-04 04:52:52 +0000

    Add `load_dataset` implementation
    This adds the `load_dataset` implementation without including it to the exported `kagglehub` module. This PR also includes:
    - Examples and documentation for how to run saved and temporary python scripts via hatch
    - Adding the `hf-datasets` optional dependencies and a hatch environment that installs them
    - Support for native hatch flags to pass through via `docker-hatch`--this also required renaming some of the ones defined in `docker-hatch` to avoid collisions
    
    http://b/379756505

--- src/kagglehub/datasets.py
+++ src/kagglehub/datasets.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Optional, Union
+from typing import Any, Optional, Union
 
 from kagglehub import registry
 from kagglehub.datasets_helpers import create_dataset_or_version
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 # Patterns that are always ignored for dataset uploading.
 DEFAULT_IGNORE_PATTERNS = [".git/", "*/.git/", ".cache/", ".huggingface/"]
+LOAD_DATASET_IMPORT_WARNING_MESSAGE = "The 'load_dataset' function requires the 'hf-datasets' extras. Install them with 'pip install kagglehub[hf-datasets]'"  # noqa: E501
 
 
 def dataset_download(handle: str, path: Optional[str] = None, *, force_download: Optional[bool] = False) -> str:
@@ -60,3 +61,32 @@ def dataset_upload(
     )
 
     create_dataset_or_version(h, tokens, version_notes)
+
+
+def load_dataset(
+    # In the form of {owner_slug}/{dataset_slug} or {owner_slug}/{dataset_slug}/versions/{version_number}
+    dataset_handle: str,
+    # Kaggle doesn't support test/train splits natively like HF does, so users
+    # would specify here. See later for why this is optional with a default.
+    train_split_percent: float = 0.8,
+    # The path to the tabular file, including the file extension.
+    # If None, use the first table determined by breadth-first search from the root of the dataset.
+    path: Optional[str] = None,
+) -> Any:  # noqa: ANN401
+    """Load a Kaggle Dataset as a Huggingface DatasetDict
+    Args:
+        handle: (string) the dataset handle
+        train_split_percent: (float) Optional split for train vs test data, defaults to 0.8
+        path: (string) Optional path to a file within a dataset
+    Returns:
+        A DatasetDict with test and train splits according to the train_split_percent
+    """
+    h = parse_dataset_handle(dataset_handle)
+
+    try:
+        import kagglehub.hf_datasets
+
+        return kagglehub.hf_datasets.load_hf_dataset(h, train_split_percent, path)
+    except ImportError:
+        # Inform the user if we detect that they didn't install everything
+        raise ImportError(LOAD_DATASET_IMPORT_WARNING_MESSAGE) from None
---
+++ tools/scripts/download_model.py
@@ -0,0 +1,3 @@
+import kagglehub
+
+print("path: ", kagglehub.model_download("google/bert/tensorFlow2/answer-equivalence-bem"))  # noqa: T201
