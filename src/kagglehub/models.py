79088c0e35172ccbf97ef3d0b1757d7c1890f530
Nesh Devanathan <neshdev@google.com>
2024-12-13 17:03:01 +0000

    Started model staging

--- src/kagglehub/models.py
+++ src/kagglehub/models.py
@@ -1,11 +1,14 @@
 import logging
+from pathlib import Path
 from typing import Optional, Union
+
+from model_signing.api import SigningConfig
 
 from kagglehub import registry
 from kagglehub.gcs_upload import normalize_patterns, upload_files_and_directories
 from kagglehub.handle import parse_model_handle
 from kagglehub.logger import EXTRA_CONSOLE_BLOCK
-from kagglehub.models_helpers import create_model_if_missing, create_model_instance_or_version
+from kagglehub.models_helpers import create_model_if_missing, create_model_instance_or_version, signing_token
 
 logger = logging.getLogger(__name__)
 
@@ -13,14 +16,19 @@ logger = logging.getLogger(__name__)
 DEFAULT_IGNORE_PATTERNS = [".git/", "*/.git/", ".cache/", ".huggingface/"]
 
 
-def model_download(handle: str, path: Optional[str] = None, *, force_download: Optional[bool] = False) -> str:
+def model_download(
+    handle: str,
+    path: Optional[str] = None,
+    *,
+    force_download: Optional[bool] = False,
+) -> str:
     """Download model files.
 
     Args:
         handle: (string) the model handle.
         path: (string) Optional path to a file within the model bundle.
         force_download: (bool) Optional flag to force download a model, even if it's cached.
-
+        publish_transparency_log: (bool) Publishes to sigstore
 
     Returns:
         A string representing the path to the requested model files.
@@ -36,6 +44,8 @@ def model_upload(
     license_name: Optional[str] = None,
     version_notes: str = "",
     ignore_patterns: Optional[Union[list[str], str]] = None,
+    *,
+    publish_transparency_log: Optional[bool] = False,
 ) -> None:
     """Upload model files.
 
@@ -51,6 +61,8 @@ def model_upload(
             https://docs.python.org/3/library/fnmatch.html.
             Use a pattern ending with "/" to ignore the whole dir,
             e.g., ".git/" is equivalent to ".git/*".
+        publish_transparency_log: (bool)
+            Creates a trasparent ledger using sigstore. User must be an admin/editor.
     """
     # parse slug
     h = parse_model_handle(handle)
@@ -58,6 +70,14 @@ def model_upload(
     if h.is_versioned():
         is_versioned_exception = "The model handle should not include the version"
         raise ValueError(is_versioned_exception)
+
+    if publish_transparency_log:
+        token = signing_token(h.owner, h.model)
+        if token:
+            SigningConfig().set_sigstore_signer(identity_token=token).sign(Path(local_model_dir), Path(local_model_dir))
+        else:
+            # skips transparency log publishing as we are unable to get a token
+            publish_transparency_log = False
 
     # Create the model if it doesn't already exist
     create_model_if_missing(h.owner, h.model)
@@ -70,5 +90,7 @@ def model_upload(
         ignore_patterns=normalize_patterns(default=DEFAULT_IGNORE_PATTERNS, additional=ignore_patterns),
     )
 
-    # Create a model instance if it doesn't exist, and create a new instance version if an instance exists
-    create_model_instance_or_version(h, tokens, license_name, version_notes)
+    if publish_transparency_log:
+        create_model_instance_or_version(
+            h, tokens, license_name, version_notes, publish_transparency_log=publish_transparency_log
+        )
