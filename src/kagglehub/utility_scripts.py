bbe77e27e91353caa6fb7197f8e403fe26ae7cd5
Johnny <chavezcalderon@google.com>
2025-01-09 00:10:25 +0000

    rd3

--- src/kagglehub/utility_scripts.py
+++ src/kagglehub/utility_scripts.py
@@ -6,7 +6,7 @@ from typing import Optional
 from kagglehub import registry
 from kagglehub.clients import KaggleApiV1Client
 from kagglehub.exceptions import KaggleApiHTTPError
-from kagglehub.handle import parse_utility_script_handle
+from kagglehub.handle import UtilityScriptHandle, parse_utility_script_handle
 from kagglehub.logger import EXTRA_CONSOLE_BLOCK
 
 logger = logging.getLogger(__name__)
@@ -29,29 +29,31 @@ def utility_script_install(handle: str, *, force_download: Optional[bool] = Fals
     logger.info(f"Downloading Utility Script: {h.to_url()} ...", extra={**EXTRA_CONSOLE_BLOCK})
     utility_script_path = registry.notebook_output_resolver(h, path=None, force_download=force_download)
 
-    if _is_notebook_utility_script(h.owner, h.notebook):
-        sys.path.append(utility_script_path)
-    else:
-        logger.info("Notebook is not an Utility Script")
+    if not _is_notebook_utility_script:
+        logger.info(
+            f"Notebook '{h.notebook}' by user '{h.notebook}is not an Utility Script"
+            "\n and will not be added to system path"
+        )
+        return utility_script_path
 
+    if utility_script_path not in sys.path:
+        sys.path.append(utility_script_path)
+
+    logger.info(f"Added {utility_script_path} to system path")
     return utility_script_path
 
 
-def _is_notebook_utility_script(user_name: str, notebook_slug: str) -> bool:
+def _is_notebook_utility_script(h: UtilityScriptHandle) -> bool:
     try:
         api_client = KaggleApiV1Client()
-
-        json_response = api_client.get(f"kernels/pull/{user_name}/{notebook_slug}")
+        json_response = api_client.get(f"kernels/pull/{h.owner}/{h.notebook}", h)
 
         category_ids = json_response["metadata"]["categoryIds"]
-        if "utility script" in category_ids:
-            return True
-        else:
-            return False
+        return "utility script" in category_ids
 
     except KaggleApiHTTPError as e:
         if e.response is not None and e.response.status_code == HTTPStatus.NOT_FOUND:
-            logger.info(f"Could not find '{notebook_slug}' metadata for user '{user_name}'.")
+            logger.info(f"Could not find '{h.owner}' metadata by user '{h.notebook}'.")
             return False
         else:
             raise (e)
