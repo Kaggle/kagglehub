869e1aa9b944565bff7fb10174b33a6090060103
mayankmalik-colab <111308679+mayankmalik-colab@users.noreply.github.com>
2025-05-09 22:23:04 +0530

    Update colab_cache_resolver.py
    Log only in case of datasets

--- src/kagglehub/colab_cache_resolver.py
+++ src/kagglehub/colab_cache_resolver.py
@@ -73,8 +73,6 @@ class ModelColabCacheResolver(Resolver[ModelHandle]):
             msg = "'slug' field missing from response"
             raise BackendError(msg)
 
-        logger.info("Using Colab cache for faster access.")
-
         base_mount_path = os.getenv(COLAB_CACHE_MOUNT_FOLDER_ENV_VAR_NAME, DEFAULT_COLAB_CACHE_MOUNT_FOLDER)
         cached_path = f"{base_mount_path}/{response['slug']}"
 
@@ -149,7 +147,7 @@ class DatasetColabCacheResolver(Resolver[DatasetHandle]):
             msg = "'slug' field missing from response"
             raise BackendError(msg)
 
-        logger.info("Using Colab cache for faster access.")
+        logger.info(f"Using Colab cache for faster access to the '{response['slug']}' dataset.")
 
         base_mount_path = os.getenv(COLAB_CACHE_MOUNT_FOLDER_ENV_VAR_NAME, DEFAULT_COLAB_CACHE_MOUNT_FOLDER)
         cached_path = f"{base_mount_path}/{response['slug']}"
